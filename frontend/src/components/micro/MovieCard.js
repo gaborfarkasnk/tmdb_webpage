618a0656d1e0c15dc9cdb2dc3a3500a3d7928dc8
Gabor Farkas <gaborfarkasnk@gmail.com>
2025-03-31 11:05:43 +0200

    json server ready, start backend

--- frontend/src/components/micro/MovieCard.js
+++ frontend/src/components/micro/MovieCard.js
@@ -3,17 +3,17 @@ import Poster from "../../images/poster.webp"
 import PopularityProgress from './PopularityProgress'
 import { Link } from 'react-router-dom'
 
-export default function MovieCard() {
+export default function MovieCard({movie_data}) {
   return (
     <Link to = "/movies/12345-mufasa" className='card movie-card'>
         <div className= 'poster-container'> 
-          <img src={Poster} alt="poster" />
+          <img src={movie_data.poster_path} alt="poster" />
           <PopularityProgress/>
         </div>
 
         <div className= 'title-container'>
-          <h4>Mufasa long title movies need three rows long title</h4>
-          <p>Sep 12, 2024</p>
+          <h4>{movie_data.title}</h4>
+          <p>{movie_data.release_date}</p>
         </div>
 
     </Link>
